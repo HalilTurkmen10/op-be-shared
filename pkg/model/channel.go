c276fc26956b76efe1f5974f70f0fadefbd9ab13
Feyza Nur Şahin <141928475+Feyzanrs@users.noreply.github.com>
2024-05-03 21:43:26 +0300

    Added createChannel and deleteChannel functionalities

--- pkg/model/channel.go
+++ pkg/model/channel.go
@@ -5,4 +5,6 @@ var (
 	ChannelLogging    string = "LOGGING"
 	ChannelCreateUser string = "CREATE_USER"
 	ChannelDeleteUser string = "DELETE_USER"
+	ChannelCreateDlr string = "CREATE_DLR"
+	ChannelDeleteDlr string = "DELETE_DLR"
 )
